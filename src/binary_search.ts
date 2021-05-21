94980a937c8e868bbd55edf886243c4e5f264265
Dmitry Vdovichenko <dmitry.vdovichencko@gmail.com>
2021-05-21 11:38:09 +0500

    finished binarySearch recursion

--- src/binary_search.ts
+++ src/binary_search.ts
@@ -1,20 +1,43 @@
 // Simple version of binarySearch
-type searchResultType = number | null;
-const binarySearch = (sortedListOfNumbers:Array<number>, item:number):searchResultType => {
-	let lowIndex:number = 0, highIndex:number = sortedListOfNumbers.length-1;
-	while (lowIndex <= highIndex) {
-		const midIndex:number = Math.floor((lowIndex + highIndex)/2), guess:number = sortedListOfNumbers[midIndex];
-		if ( guess === item ){
-			return midIndex;
-		}
-		else if (guess > item){
-			highIndex = midIndex - 1;
-		}
-		else {
-			lowIndex = midIndex +1;
-		}
-	}
-	return null;
-}
+type SearchResultType = number | null;
+const binarySearch = (sortedListOfNumbers: Array<number>, item: number): SearchResultType => {
+  let lowIndex = 0;
+  let highIndex: number = sortedListOfNumbers.length - 1;
+  while (lowIndex <= highIndex) {
+    const midIndex: number = Math.floor((lowIndex + highIndex) / 2);
+    const guess: number = sortedListOfNumbers[midIndex];
+    if (guess === item) {
+      return midIndex;
+    }
+    if (guess > item) {
+      highIndex = midIndex - 1;
+    } else {
+      lowIndex = midIndex + 1;
+    }
+  }
+  return null;
+};
+
+// Recursion version of binarySearch
+const binarySearchRecursion = (
+  sortedListOfNumbers: Array<number>,
+  item: number,
+  lowIndex = 0,
+  highIndex: number = sortedListOfNumbers.length - 1,
+): SearchResultType => {
+  const midIndex: number = Math.floor((lowIndex + highIndex) / 2);
+  const guess: number = sortedListOfNumbers[midIndex];
+  if (guess === item) {
+    return midIndex;
+  }
+  const newHighIndex = guess > item ? midIndex - 1 : highIndex;
+  const newLowIndex = guess < item ? midIndex + 1 : lowIndex;
+  if (lowIndex <= highIndex) {
+    return binarySearchRecursion(sortedListOfNumbers, item, newLowIndex, newHighIndex);
+  }
+  return null;
+};
+
+export { binarySearchRecursion };
 
 export default binarySearch;
--- test/binary_search.test.ts
+++ test/binary_search.test.ts
@@ -1,5 +1,6 @@
 import binary_search from "../src/binary_search";
-describe("Test binary search algorithm", () => {
+import { binarySearchRecursion } from '../src/binary_search';
+describe("Test simple binary search algorithm", () => {
   test("search for item in sorted Array must return index or null", () => {
     const list = [1, 3, 5, 7, 9],
       item = 7,
@@ -8,5 +9,16 @@ describe("Test binary search algorithm", () => {
     expect(binary_search(list, item)).toBe(3);
     expect(binary_search(list, firstItem)).toBe(0);
     expect(binary_search(list, nonExistItem)).toBe(null);
+  });
+});
+describe("Test recursion binary search algorithm", () => {
+  test("search for item in sorted Array must return index or null", () => {
+    const list = [1, 3, 5, 7, 9],
+      item = 7,
+      firstItem = list[0],
+      nonExistItem = 2;
+    expect(binarySearchRecursion(list, item)).toBe(3);
+    expect(binarySearchRecursion(list, firstItem)).toBe(0);
+    expect(binarySearchRecursion(list, nonExistItem)).toBe(null);
   });
 });
